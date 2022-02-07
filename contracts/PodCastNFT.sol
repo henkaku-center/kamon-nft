d95283174900e5a7e0ead469d7df73fd8b20d20c
SamuraiT <t.yasukawa01@gmail.com>
2022-02-07 23:54:13 +0900

    Add consensus voting mechanism
    Signed-off-by: SamuraiT <t.yasukawa01@gmail.com>

--- contracts/PodCastNFT.sol
+++ contracts/PodCastNFT.sol
@@ -8,13 +8,56 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract PodCastNFT is ERC721, AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    constructor(address[] memory _admins) ERC721("Henkaku NFT", "henkaku") {
-      addAdmins(_admins);
+    bytes32 public constant ADMIN = keccak256("ADMIN");
+    bytes32 public constant UPVOTE_FOR_ADMIN = keccak256("UPVOTE_FOR_ADMIN");
+    bytes32 public constant UPVOTE_FOR_WITHDRAW = keccak256("UPVOTE_FOR_WITHDRAW");
+
+    struct Vote {
+      mapping(address => bool) voter;
+      uint256 votes;
     }
 
-    function addAdmins(address[] memory _admins) private {
+    uint private _numberOfAdmins;
+    mapping(bytes32 => mapping(address => Vote)) private votes;
+
+    constructor(address[] memory _admins) ERC721("Henkaku NFT", "henkaku") {
+      //_setRoleAdmin(ADMIN_ROLE, DEFAULT_ADMIN_ROLE);
+      addInitialAdmins(_admins);
+    }
+
+    function _hasConsensus(address _candidate, bytes32 _role) public view returns (bool) {
+      return votes[_role][_candidate].votes >= _numberOfAdmins / 3;
+    }
+
+    function acceptAsAdmin(address _candidate) public {
+        Vote storage _vote = votes[UPVOTE_FOR_ADMIN][_candidate];
+        require(!_vote.voter[msg.sender], "You cannot vote more than once");
+        _vote.voter[msg.sender] = true;
+        _vote.votes ++;
+
+        if (_hasConsensus(_candidate, UPVOTE_FOR_ADMIN)) {
+          _grantRole(ADMIN_ROLE, _candidate);
+          _numberOfAdmins ++;
+        }
+    }
+
+    function acceptWithdraw(address _candidate) public {
+        Vote storage _vote = votes[UPVOTE_FOR_WITHDRAW][_candidate];
+        require(!_vote.voter[msg.sender], "You cannot vote more than once");
+        _vote.voter[msg.sender] = true;
+        _vote.votes ++;
+
+        if (_hasConsensus(_candidate, UPVOTE_FOR_WITHDRAW)) {
+          _revokeRole(ADMIN_ROLE, _candidate);
+          _numberOfAdmins --;
+        }
+    }
+
+    function addInitialAdmins(address[] memory _admins) private {
       for (uint i=0; i < _admins.length; i++) {
-        _setupRole(ADMIN_ROLE, _admins[i]);
+        _grantRole(ADMIN_ROLE, _admins[i]);
+        _grantRole(DEFAULT_ADMIN_ROLE, _admins[i]);
+        _numberOfAdmins ++;
       }
     }
 
---
+++ scripts/admin.js
@@ -0,0 +1,42 @@
+const hre = require("hardhat");
+
+const main = async () => {
+  const [admin1, admin2, user1, user2] = await hre.ethers.getSigners()
+  const podcastNFTContract = await hre.ethers.getContractFactory("PodCastNFT");
+  const contract = await podcastNFTContract.deploy([admin1.address, admin2.address]);
+
+  await contract.deployed();
+
+  let txn;
+  txn = await contract.connect(admin1).updateNFT(1)
+  await txn.wait()
+
+  txn = await contract.connect(admin2).updateNFT(1)
+  await txn.wait()
+  // use admin
+  //const adminRole = hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("ADMIN_ROLE"))
+  //txn = await contract.connect(admin1).grantRole(adminRole, user1.address)
+  //await txn.wait()
+
+  txn = await contract.connect(admin1).acceptAsAdmin(user1.address)
+  await txn.wait()
+  txn = await contract.connect(user1).updateNFT(1)
+  await txn.wait()
+
+  txn = await contract.connect(admin1).acceptWithdraw(user1.address)
+  await txn.wait()
+
+  txn = await contract.connect(user1).updateNFT(1)
+  await txn.wait() // fail
+
+  txn = await contract.connect(user2).updateNFT(1)
+  await txn.wait() // fail
+  console.log("deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
