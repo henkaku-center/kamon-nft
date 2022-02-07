61cf4a279b6f47abbe3afabb32dd8c998b3d7889
SamuraiT <t.yasukawa01@gmail.com>
2022-02-07 21:22:17 +0900

    Add dot env for future deployment
    Signed-off-by: SamuraiT <t.yasukawa01@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -17,5 +17,5 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.4"
 };
