e93ba34bd61dce6cb3411d5e0045e7f7c0e68d65
Masumi Kawasaki <geeknees@gmail.com>
2022-02-10 22:20:47 +0900

    Merge and run formatter

--- contracts/PodCastNFT.sol
+++ contracts/PodCastNFT.sol
@@ -7,45 +7,56 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ConsensusAdminable.sol";
 
-contract PodCastNFT is ERC721,  IERC721Receiver, ConsensusAdminable {
+contract PodCastNFT is ERC721, IERC721Receiver, ConsensusAdminable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor(address[] memory _admin, bool givenHighestAuthority)
-      ConsensusAdminable(_admin, givenHighestAuthority)
-      ERC721("Henkaku NFT", "henkaku")  {
-
-    }
+        ConsensusAdminable(_admin, givenHighestAuthority)
+        ERC721("Henkaku NFT", "henkaku")
+    {}
 
     receive() external payable {
-      console.log(msg.value);
-      console.log("receive");
+        console.log(msg.value);
+        console.log("receive");
     }
 
-
     fallback() external payable {
-      console.log(msg.value);
-      console.log("fallback");
+        console.log(msg.value);
+        console.log("fallback");
     }
 
     function updateNFT(uint256 tokenId) public {
-      require(hasRole(ADMIN_ROLE, msg.sender), "You are not authorized to update nft");
-      //TODO update nft
-      console.log("Wow epic!!");
+        require(
+            hasRole(ADMIN_ROLE, msg.sender),
+            "You are not authorized to update nft"
+        );
+        //TODO update nft
+        console.log("Wow epic!!");
     }
 
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
         return
             interfaceId == type(IERC721).interfaceId ||
             interfaceId == type(IERC721Metadata).interfaceId ||
             super.supportsInterface(interfaceId);
+    }
 
-        // TODO: 管理者のみ実行可能にする必要あり
-    function mintAndTransfer(address _to) public returns (uint256){
+    function mintAndTransfer(address _to) public returns (uint256) {
+        require(
+            hasRole(ADMIN_ROLE, msg.sender),
+            "You are not authorized to mint"
+        );
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
