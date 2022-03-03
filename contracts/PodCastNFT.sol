4a66090bd5b455d1b0819dbd67b20d797338b7e4
Masumi Kawasaki <geeknees@gmail.com>
2022-03-03 22:58:53 +0900

    make Ownable to edit collection

--- contracts/PodCastNFT.sol
+++ contracts/PodCastNFT.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ConsensusAdminable.sol";
 
 import {Base64} from "./libraries/Base64.sol";
 
-contract PodCastNFT is ERC721URIStorage, ConsensusAdminable {
+contract PodCastNFT is ERC721URIStorage, Ownable, ConsensusAdminable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
