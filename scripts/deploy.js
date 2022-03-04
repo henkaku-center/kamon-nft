0bf39592ccc1407f4d3eb90258f8923ad52465a4
Masumi Kawasaki <geeknees@gmail.com>
2022-03-04 17:57:06 +0900

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+const main = async () => {
+  const podcastNFTContract = await hre.ethers.getContractFactory("PodCastNFT");
+  const contract = await podcastNFTContract.deploy(["ADMIN_ADDRESS"], false);
+
+  await contract.deployed();
+  console.log("deployed to:", contract.address);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
