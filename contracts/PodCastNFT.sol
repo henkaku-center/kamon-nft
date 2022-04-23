8432756e66461bca78f1a23e76c3365448ffaf66
RYU <96900106+RyuKiyo66@users.noreply.github.com>
2022-04-23 15:49:07 +0900

    Update PodCastNFT.sol
    importと、ERC721Enumerableの継承を追加しました。

--- contracts/PodCastNFT.sol
+++ contracts/PodCastNFT.sol
@@ -6,10 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ConsensusAdminable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import {Base64} from "./libraries/Base64.sol";
 
-contract PodCastNFT is ERC721URIStorage, Ownable, ConsensusAdminable {
+contract PodCastNFT is ERC721URIStorage, ERC721Enumerable, Ownable, ConsensusAdminable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
