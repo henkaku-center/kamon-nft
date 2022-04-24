1f2902ee6f0eeb5bec34e5e6bea9e616453e9b63
Masumi Kawasaki <geeknees@gmail.com>
2022-04-25 01:48:31 +0900

    remove ERC721URIStorage

--- contracts/PodCastNFT.sol
+++ contracts/PodCastNFT.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import {IERC20} from "./interface/IERC20.sol";
 
-contract PodCastNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
+contract PodCastNFT is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     IERC20 public henkakuToken;
     uint256 public price;
     address public fundAddress;
+    string public baseTokenURI;
     bytes32 private keyword;
 
     struct PodcastKeyword {
@@ -42,37 +42,12 @@ contract PodCastNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
         setFundAddress(_fundAddress);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
+    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
     }
 
     function setPrice(uint256 _price) public onlyOwner {
@@ -134,62 +109,18 @@ contract PodCastNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
         roles[_to].push(_role);
     }
 
-    // TODO implement updateNFT func which holder can change their name, imageURL by them self
-    function updateOwnNFT(string memory _imageURI, string memory name) public {}
-
-    function updateNFT(
-        uint256 tokenId,
-        string memory _imageURI,
-        string[] memory _roles,
-        string memory _point
-    ) public onlyOwner {
-        string memory finalTokenUri = getTokenURI(
-            tokenId,
-            _imageURI,
-            _roles,
-            _point
-        );
-        _setTokenURI(tokenId, finalTokenUri);
-    }
-
-    function getTokenURI(
-        uint256 _tokenId,
-        string memory _imageURI,
-        string[] memory _roles,
-        string memory _point
-    ) internal view returns (string memory) {
-        // TODO
-        return "";
-    }
-
-    function mint(
-        string memory _imageURI,
-        string[] memory _roles,
-        string memory _point,
-        address _to
-    ) public onlyOwner returns (uint256) {
+    function mint(address _to) public onlyOwner returns (uint256) {
         require(balanceOf(_to) == 0, "User has had already a membership NFT");
-        return _mint(_imageURI, _roles, _point, _to);
+        _mint(_to);
     }
 
-    function _mint(
-        string memory _imageURI,
-        string[] memory _roles,
-        string memory _point,
-        address _to
-    ) internal onlyNoneHolder(msg.sender) returns (uint256) {
-        _tokenIds.increment();
+    function _mint(address _to)
+        internal
+        onlyNoneHolder(msg.sender)
+        returns (uint256)
+    {
         uint256 newItemId = _tokenIds.current();
         _safeMint(_to, newItemId);
-        roles[_to] = _roles;
-        // TODO implement getTokenURI with _roles
-        string memory finalTokenUri = getTokenURI(
-            newItemId,
-            _imageURI,
-            _roles,
-            _point
-        );
-        _setTokenURI(newItemId, finalTokenUri);
         return newItemId;
     }
 
