6a9283dbc616f3165755fff7cacb534e91f48e6f
SamuraiT <t.yasukawa01@gmail.com>
2022-04-26 22:19:48 +0900

    feat: Introduce hardhat-contract-sizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require('hardhat-contract-sizer');
 require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
